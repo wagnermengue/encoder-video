1011642324c66d55459cc886a88dd962511490f2
Wagner Mengue <dev.mengue@gmail.com>
2022-07-05 23:32:51 -0300

    adjust connection db

--- domain/video.go
+++ domain/video.go
@@ -1,15 +1,17 @@
 package domain
 
 import (
-	"github.com/asaskevich/govalidator"
 	"time"
+
+	"github.com/asaskevich/govalidator"
 )
 
 type Video struct {
-	ID         string    `valid:"uuid"`
-	ResourceID string    `valid:"notnull"`
-	FilePath   string    `valid:"notnull"`
-	CreatedAt  time.Time `valid:"-"`
+	ID         string    `json:"encoded_video_folder" valid:"uuid" gorm:"type:uuid;primary"`
+	ResourceID string    `json:"resource_id" valid:"notnull" gorm:"type:varchar(255)"`
+	FilePath   string    `json:"file_path" valid:"notnull" gorm:"type:varchar(255)"`
+	CreatedAt  time.Time `json:"-" valid:"-"`
+	Jobs       []*Job    `json:"-" valid:"-" gorm:"ForeignKet:VideoID"`
 }
 
 func init() {
